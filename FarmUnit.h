10821cc455d611010228d2dbd06318da2059753a
Jay-9-9-9 <u22625926@tuks.co.za>
2024-09-05 11:48:40 +0200

    Adding defintions for functions

--- FarmUnit.h
+++ FarmUnit.h
@@ -1,8 +1,17 @@
 #ifndef FARMUNIT_H
 #define FARMUNIT_H
 
-class FarmUni{
+#include <string>
+using namespace std ;
 
+class FarmUni{
+     public :
+        virtual int getTotalCapacity() = 0 ;
+        virtual string getCropType() = 0 ;
+        virtual string getSoilStateName() = 0 ; 
+        virtual int getCurrentStoredCrops() = 0 ;
+        virtual void storeCrops(int amount) = 0 ;
+        virtual void removeCrops(int amount) = 0 ;
 } ;
 
 #endif
