66b603770cb7ee62f96cc09ff736f4705eb40efb
Jay-9-9-9 <u22625926@tuks.co.za>
2024-09-05 12:26:13 +0200

    new Barn class and definitions

---
+++ Barn.h
@@ -0,0 +1,22 @@
+#ifndef BARN_H
+#define BARN_H
+
+#include "FarmUnit.h"
+
+#include <string>
+#include <iostream>
+using namespace std ;
+
+class Barn : public FarmUnit{
+    private :
+        int BarnCapacity ;
+        int storedAmount ;
+        string BarnCropType ;
+    public :
+        int getTotalCapacity() override ;
+        int getCurrentStoredCrops() override ;
+        void storeCrops(int amount) override ;
+        void removeCrops(int amount) override ;
+}; 
+
+#endif
